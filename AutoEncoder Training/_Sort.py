e2b4d3ad8b7ab7dcb7e036f14ef3c433d5713ed0
Vincent He <vincent0710@Outlook.com>
2020-09-19 12:14:06 -0400

    Feature Add: file Move image and scale image all in one.

--- AutoEncoder Training/_Sort.py
+++ AutoEncoder Training/_Sort.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 from PIL import Image
+DIRNAME = "cropped"
 
 
 def make_folder(path):
@@ -12,10 +13,11 @@ def make_folder(path):
 
 Save_Train = os.path.join(os.curdir, "Train", "Face")
 Save_Validation = os.path.join(os.curdir, "Validation", "Face")
-image_dir = os.path.join(os.curdir, "Original Image")
+image_dir = os.path.join(os.curdir, "Original Image", DIRNAME)
 
 make_folder(Save_Train)
 make_folder(Save_Validation)
+make_folder(image_dir)
 
 
 def open_image(path):
@@ -26,29 +28,41 @@ def open_image(path):
         else:
             pass
     except Exception as e:
-        print(f"UNKNOWN ERROR {path}, {e}")
+        print(f"Cannot Open {path}, Error Message: {e}")
 
 
 def open_images(path):
     files = [os.path.join(path, name) for name in os.listdir(path)]
     count = 0
     for file in files:
-
-        if ".jpg" in file:
-            img = open_image(file)
-            if img:
-                move(file, os.path.join(Save_Train, f"Anime_Face{count}.jpg"))
-                count += 1
-
-                break
+        img = open_image(file)
+        if img:
+            move(file, os.path.join(Save_Train, f"Anime_Face{count}.jpg"))
+            count += 1
 
 
 def move(p1, p2):
     shutil.move(p1, p2)
 
 
+def make_validation(img_path, move_dir):
+    files = [os.path.join(img_path, name) for name in os.listdir(img_path)]
+    count = 0
+    for index in range(int(len(files) * 0.1)):
+        move(files[index], os.path.join(move_dir, f"Face {count} .jpg"))
+        count += 1
+
+
+def remove_dir_files(path):
+    files = [os.path.join(path, name) for name in os.listdir(path)]
+    for file in files:
+        os.remove(file)
+
+
 def main():
-    open_image(image_dir)
+    open_images(image_dir)
+    make_validation(Save_Train, Save_Validation)
+    remove_dir_files(image_dir)  # Getting rid of unwanted image
 
 
 main()
